3b84c8546197b0b93e3bfab0a3a76deb05d95063
Rasheed Rahman <ra1da35ma@gmail.com>
2018-09-24 21:20:05 +0100

    Documented function using JsDoc
    Signed-off-by: Rasheed Rahman <ra1da35ma@gmail.com>

--- index.js
+++ index.js
@@ -2,29 +2,28 @@ const csv = require("fast-csv");
 // const fetch = require("node-fetch");
 const fs = require("fs");
 
-/*
-* async function to write data to a file
-*
-* @param path string
-* @param data any
-*
-* @return Promise<null>
-* */
-function writeFileAsync(path, data) {
+/**
+ * Async function to write data to a file
+ *
+ * @param parameters
+ * @return Promise
+ */
+function writeFileAsync(parameters) {
+	let {path, data} = parameters;
 	return new Promise(function (resolve, reject) {
 		fs.writeFile(path, data, function (error) {
 			if (error) reject(error);
-			resolve();
+			return resolve();
 		});
 	});
 }
 
-const doTask = () => {
+const main = () => {
 	return new Promise((resolve, reject) => {
 		csv.fromPath('file.csv')
 				.transform(data => data.reverse())
 				.on('data', response => {
-					writeFileAsync("result.csv", response)
+					writeFileAsync({path: "result.csv", data: response})
 							.then(() => resolve('DONE!'))
 							.catch(error => reject(error));
 				})
@@ -48,5 +47,5 @@ const doTask = () => {
 	});
 };*/
 
-doTask();
+main();
 // checkAbuse('https://radiumrasheed.tech');
